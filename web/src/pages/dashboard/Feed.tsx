ae7cbef1f73df07077b99f35dbb452dcd7cdd299
M Gilang Januar <mgilangjanuar@gmail.com>
2021-08-06 14:31:34 +0700

    implement update feed

--- web/src/pages/dashboard/Feed.tsx
+++ web/src/pages/dashboard/Feed.tsx
@@ -1,14 +1,17 @@
 import { CommentOutlined, DeleteOutlined, EditOutlined, HeartFilled, HeartOutlined } from '@ant-design/icons'
-import { Button, Card, Col, Layout, Popconfirm, Row, Space, Tooltip, Typography } from 'antd'
+import { Button, Card, Col, Drawer, Form, Input, Layout, message, Popconfirm, Row, Select, Space, Tooltip, Typography } from 'antd'
 import Avatar from 'antd/lib/avatar/avatar'
+import { useForm } from 'antd/lib/form/Form'
 import moment from 'moment'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { RouteComponentProps, useHistory } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import Navigation from '../../components/Navigation'
 import { useDelete } from '../../hooks/feed/useDelete'
 import { useLike } from '../../hooks/feed/useLike'
 import { useRetrieve } from '../../hooks/feed/useRetrieve'
+import { useUpdate } from '../../hooks/feed/useUpdate'
+import { useFind } from '../../hooks/pet/useFind'
 import { useMe } from '../../hooks/useMe'
 
 interface PageProps extends RouteComponentProps<{
@@ -19,13 +22,18 @@ const Feed: React.FC<PageProps> = ({ match }) => {
   const history = useHistory()
   const [user] = useMe()
   const [retrieve, feed, error, reset] = useRetrieve()
+  const [update, errorUpdate, resetUpdate] = useUpdate()
   const [like, feedLike] = useLike()
   const [remove, errorRemove] = useDelete()
+  const [showDrawer, setShowDrawer] = useState<boolean>()
+  const [form] = useForm()
+  const [findPets, pets] = useFind()
 
   useEffect(() => {
     if (user === null) {
-      history.replace('/')
+      return history.replace('/')
     }
+    findPets({ owner: user?.email })
   }, [history, user])
 
   useEffect(() => {
@@ -33,6 +41,10 @@ const Feed: React.FC<PageProps> = ({ match }) => {
       reset()
     }
   }, [feedLike])
+
+  useEffect(() => {
+    form.setFieldsValue(feed)
+  }, [feed])
 
   useEffect(() => {
     if (error === undefined) {
@@ -46,6 +58,19 @@ const Feed: React.FC<PageProps> = ({ match }) => {
     }
   }, [errorRemove])
 
+  useEffect(() => {
+    if (errorUpdate === null) {
+      message.success('Updated')
+      setShowDrawer(false)
+      reset()
+    }
+    resetUpdate()
+  }, [errorUpdate])
+
+  const finish = () => {
+    update(feed.id, form.getFieldsValue())
+  }
+
   return <>
     <Navbar />
     <Layout style={{ flexDirection: 'row' }}>
@@ -57,7 +82,7 @@ const Feed: React.FC<PageProps> = ({ match }) => {
               <Button key="comment" type="text" icon={<CommentOutlined />}></Button>
             ]} extra={feed.owner === user?.email ? <Space>
               <Tooltip title="Edit">
-                <Button icon={<EditOutlined />} type="text" shape="circle" />
+                <Button onClick={() => setShowDrawer(true)} icon={<EditOutlined />} type="text" shape="circle" />
               </Tooltip>
               <Tooltip title="Delete">
                 <Popconfirm title="Are you sure to delete this?" onConfirm={() => remove(feed?.id)}>
@@ -79,6 +104,21 @@ const Feed: React.FC<PageProps> = ({ match }) => {
         </Row>
       </Layout.Content>
     </Layout>
+    <Drawer title="Update" closable placement="right" visible={showDrawer} onClose={() => setShowDrawer(false)}>
+      <Form layout="vertical" form={form} onFinish={finish}>
+        <Form.Item wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} label="Pet" name="pet_id" rules={[{ required: true, message: 'Please select the pet' }]}>
+          <Select size="large">
+            {pets?.map(pet => <Select.Option value={pet.id}><Avatar src={pet.avatar_url} style={{ marginRight: '10px' }} /> {pet.name}</Select.Option>)}
+          </Select>
+        </Form.Item>
+        <Form.Item name="caption" label="Caption">
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item style={{ marginTop: '20px', float: 'right' }}>
+          <Button type="primary" icon={<EditOutlined />} htmlType="submit">Update</Button>
+        </Form.Item>
+      </Form>
+    </Drawer>
     <Navigation page="main" />
   </>
 }
